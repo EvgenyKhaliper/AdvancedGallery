adb268d25435e0c389d207fd8e1bf57bbf0bde01
@vadim17 <vadim@genesort.com>
2020-06-14 14:16:24 +0300

    first commit

--- gallery/components/sierch/Sierch.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Sierch = () => {
-  return <div>Sierch</div>;
-};
-
-export default Sierch;
--- gallery/src/App.js
+++ gallery/src/App.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Search from "./components/search";
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:3001"
+});
 
 function App() {
+  const [data, getData] = useState([]);
+  const [gallery, setGallery] = useState([]);
+  
+  useEffect(async () => {
+    const res = await instance.get("/");
+    console.log(res.data)
+    getData(res.data)
+  }, []);
+  
+  useEffect(async () => {
+    if (data) {
+      const res = await instance.get(`https://pixabay.com/api/?key=17036938-0f3bf7eb3cc6b295a8a503e34&q=${getStrOfQueries(data)}`);
+      console.log(res.data)
+      setGallery(res.data)
+    }
+  }, [data]);
+  
+  const getStrOfQueries = arr => {
+    let queries;
+    console.log({arr})
+    arr.forEach(query => {
+      console.log(query.query)
+      if (!queries) {
+        queries = `${query.query}`
+      }
+      else {
+        queries += `+${query.query}`
+      }
+    })
+    console.log({queries})
+    return queries;
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         Advanced Gallery
+        <Search/>
       </header>
     </div>
   );
