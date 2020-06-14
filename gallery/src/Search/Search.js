cb67ff09b95983a9f4ea19c467fd49ff23189e65
DESKTOP-F5QML8E\Alex <golan4ik@gmail.com>
2020-06-14 14:33:31 +0300

    Search added

---
+++ gallery/src/Search/Search.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { GET_QUERY_URL } from "../constants";
+
+const Search = (props) => {
+  return null;
+};
+
+export default Search;
--- gallery/src/constants.js
+++ gallery/src/constants.js
@@ -1,1 +1,3 @@
 export const API_KEY = '17038189-0561271b1cf24beda56724eb2';
+export const SAVE_QUERY_URL = 'localhost:3000/';
+export const GET_QUERY_URL = 'localhost:3000/';
